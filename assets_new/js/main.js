a0e8e2e57e3234648d8ee5ae4e8758147c12aba4
yutingh <yutingh@cw.com.tw>
2021-01-28 15:49:47 +0800

    上線後跑版修正
    h1超過圖片會被廣告蓋掉
    .subtitle-block 沒樣式
    新字型在 css 連結斷了
    複製連結按鈕失效
    cookies 顯示

--- assets_new/js/main.js
+++ assets_new/js/main.js
@@ -172,3 +172,66 @@ $(function () {
 		$("body > .banner img, .bottom-banner img").attr('src', 'https://fakeimg.pl/300x50/?retina=3').width(300);
 	}
 });
+$(function(){
+    // 複製網址
+    document.getElementById("shareCopy").addEventListener("click", function() {
+        copyToClipboard(document.getElementById("copyTarget"));
+    });
+    document.getElementById("shareCopyBottom").addEventListener("click", function() {
+        copyToClipboard(document.getElementById("copyTargetBottom"));
+    });
+    $("#shareCopy, #shareCopyBottom").click(function(){
+        $(this).siblings().fadeIn();
+        setTimeout(function(){
+            $(".copy-success").fadeOut();
+        }, 1500);
+        return false;
+    })
+    function copyToClipboard(elem) {
+        // create hidden text element, if it doesn't already exist
+        var targetId = "_hiddenCopyText_";
+        var isInput = elem.tagName === "INPUT" || elem.tagName === "TEXTAREA";
+        var origSelectionStart, origSelectionEnd;
+        if (isInput) {
+            // can just use the original source element for the selection and copy
+            target = elem;
+            origSelectionStart = elem.selectionStart;
+            origSelectionEnd = elem.selectionEnd;
+        } else {
+            // must use a temporary form element for the selection and copy
+            target = document.getElementById(targetId);
+            if (!target) {
+                var target = document.createElement("textarea");
+                target.style.position = "absolute";
+                target.style.left = "-9999px";
+                target.style.top = "0";
+                target.id = targetId;
+                document.body.appendChild(target);
+            }
+            target.textContent = elem.textContent;
+        }
+        // select the content
+        var currentFocus = document.activeElement;
+        target.focus();
+        target.setSelectionRange(0, target.value.length);
+        // copy the selection
+        var succeed;
+        try {
+            succeed = document.execCommand("copy");
+        } catch (e) {
+            succeed = false;
+        }
+        // restore original focus
+        if (currentFocus && typeof currentFocus.focus === "function") {
+            currentFocus.focus();
+        }
+        if (isInput) {
+            // restore prior selection
+            elem.setSelectionRange(origSelectionStart, origSelectionEnd);
+        } else {
+            // clear temporary content
+            target.textContent = "";
+        }
+        return succeed;
+    }
+});
